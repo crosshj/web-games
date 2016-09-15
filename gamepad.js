8324b4fcbd15289053827fc4d27fcf62473326c4
crosshj <github@chimpjuice.com>
2016-09-14 23:25:01 -0400

    Update gamepad.js

--- gamepad.js
+++ gamepad.js
@@ -1,10 +1,12 @@
 function checkGamepad(){
+  var isSupported = false;
   if(navigator.getGamepads){
     var message = "gamepad api IS supported";
     var node = document.createElement("div");
     var textnode = document.createTextNode(message);
     node.appendChild(textnode);
     document.body.appendChild(node);
+    isSupported = true;
   } else {
     var message = "gamepad api NOT supported";
     var node = document.createElement("div");
@@ -12,8 +14,15 @@ function checkGamepad(){
     node.appendChild(textnode);
     document.body.appendChild(node);
   }
+  return isSupported;
 }
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-  checkGamepad();
+  if (checkGamepad()){
+    var gamepads = navigator.getGamepads();
+  }
 });
+
+//meh - do it later
+
+	
