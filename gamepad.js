b4f0b5e2e59895d6169711e859f8eee9130840c7
crosshj <github@chimpjuice.com>
2016-09-14 23:09:42 -0400

    append to doc body on gamepad api support

--- gamepad.js
+++ gamepad.js
@@ -1,1 +1,13 @@
-alert('this happened')
+if(!navigator.getGamepads){
+  var message = "gamepad api IS supported";
+  var node = document.createElement("div");
+  var textnode = document.createTextNode(message);
+  node.appendChild(textnode);
+  document.body.appendChild(node);
+} else {
+  var message = "gamepad api NOT supported";
+  var node = document.createElement("div");
+  var textnode = document.createTextNode(message);
+  node.appendChild(textnode);
+  document.body.appendChild(node);
+}
